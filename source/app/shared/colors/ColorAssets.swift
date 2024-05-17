f7635f2db16f2457a9c3c2dec03ce506a2970c67
Tiago Amaral <tiagofly@outlook.com>
2024-05-17 00:31:14 -0300

    Update ColorAssets.swift Ajuste de paleta de cores

---
+++ source/app/shared/colors/ColorAssets.swift
@@ -0,0 +1,14 @@
+//
+//  ColorAssets.swift
+//  dictWordEnglish
+//
+//  Created by Tiago Amaral on 16/05/24.
+//
+
+import UIKit
+
+struct ColorAssets {
+    static let mainColor = UIColor(named: "mainColor")
+    static let secondColor = UIColor(named: "secondColor")
+    static let mainTextColor = UIColor(named: "mainTextColor")
+}
