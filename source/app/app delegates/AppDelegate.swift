ab212621f59fc1b6a55c6713091d549a1963ed88
Tiago Amaral <tiagofly@outlook.com>
2024-05-17 18:05:40 -0300

    Update AppDelegate.swift Refactor para novo metodo de inicializacao

--- source/app/app delegates/AppDelegate.swift
+++ source/app/app delegates/AppDelegate.swift
@@ -15,9 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         RealmMigrationManager.executeMigration()
-        
         window = UIWindow(frame: UIScreen.main.bounds)
-        SceneStarter.makeScene(into: window)
+        SceneStarterManager.startView(into: window)
         return true
     }
 }
