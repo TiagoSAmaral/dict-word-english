a9f577c023c4519229827b3cdc4792fb9b3eaa34
Tiago Amaral <tiagofly@outlook.com>
2024-05-17 18:08:36 -0300

    Update NetworkAPIWorkerIntegration.swift and NetworkAPIWorker.swift Conformidade de Worker de requisicao

---
+++ source/app/scenes/list scene/worker integrations/NetworkAPIWorkerIntegration.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkAPIWorkerIntegration.swift
+//  dictWordEnglish
+//
+//  Created by Tiago Amaral on 17/05/24.
+//
+
+import Foundation
+
+extension NetworkAPIWorker: ApiRequestable {
+
+}
---
+++ source/app/shared/workers/network/NetworkAPIWorker.swift
@@ -0,0 +1,10 @@
+//
+//  networkAPIWorker.swift
+//  dictWordEnglish
+//
+//  Created by Tiago Amaral on 17/05/24.
+//
+
+import Foundation
+
+class NetworkAPIWorker: NetworkConectable {}
