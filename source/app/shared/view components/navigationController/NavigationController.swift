d2582556d39ae0a4ffe675824248b9059839428e
Tiago Amaral <tiagofly@outlook.com>
2024-05-17 00:30:58 -0300

    Update NavigationController.swift Adicionado navigation controller com ajuste para corrigir bug de navigationBar e ajuste de cor para modo escuro

--- source/app/shared/view components/navigationController/NavigationController.swift
+++ source/app/shared/view components/navigationController/NavigationController.swift
@@ -8,4 +8,35 @@
 import UIKit
 
 class NavigationController: UINavigationController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        defineApperance()
+        fixBartintColoriOS15()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        defineApperance()
+        fixBartintColoriOS15()
+    }
+    
+    func defineApperance() {
+        navigationBar.isTranslucent = false
+        defineBackbuttonApperance()
+    }
+    
+    private func fixBartintColoriOS15() {
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = ColorAssets.mainColor
+            navigationBar.standardAppearance = appearance
+            navigationBar.scrollEdgeAppearance = navigationBar.standardAppearance
+        }
+    }
+    
+    func defineBackbuttonApperance() {
+        navigationBar.barTintColor = ColorAssets.mainColor
+    }
 }
