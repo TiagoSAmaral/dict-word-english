9ca89dd28c3f2ee94ea60edddc147e4d649782c2
Tiago Amaral <tiagofly@outlook.com>
2024-05-17 00:32:28 -0300

    Update Empty.swift Extension para String vazia.

---
+++ source/app/shared/extensions/string/Empty.swift
@@ -0,0 +1,15 @@
+//
+//  Empty.swift
+//  dictWordEnglish
+//
+//  Created by Tiago Amaral on 16/05/24.
+//
+
+import Foundation
+
+extension String {
+    
+    static var empty: String {
+        ""
+    }
+}
