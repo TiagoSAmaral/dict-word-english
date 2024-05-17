73749b4b69080df0907a185701bacca38aa0bca0
Tiago Amaral <tiagofly@outlook.com>
2024-05-17 00:34:30 -0300

    Update DetailController.swift and DetailSceneFactory.swift Adicionado arquivos para Detail Scene

---
+++ source/app/scenes/detail scene/DetailController.swift
@@ -0,0 +1,8 @@
+//
+//  DetailController.swift
+//  dictWordEnglish
+//
+//  Created by Tiago Amaral on 16/05/24.
+//
+
+import Foundation
---
+++ source/app/scenes/detail scene/DetailSceneFactory.swift
@@ -0,0 +1,8 @@
+//
+//  DetailFactory.swift
+//  dictWordEnglish
+//
+//  Created by Tiago Amaral on 16/05/24.
+//
+
+import Foundation
